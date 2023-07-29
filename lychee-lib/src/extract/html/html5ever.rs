0732b09cb8c741e4ba5206293b7ac2ba7ed8891f
Matthias <matthias-endler@gmx.net>
2023-07-30 00:40:13 +0200

    Don't check preconnect links
    Preconnect links are used to establish a server connection without loading a
    specific resource yet. Not always do these links point to a URL that should
    return a 200, and they are not user-facing, i.e. they don't show up in the
    final rendered version of a page.
    
    Therefore, I think we should them at all; not even in `--include-verbatim`
    mode, as they might not point to a valid resource.
    
    Fixes #897

--- lychee-lib/src/extract/html/html5ever.rs
+++ lychee-lib/src/extract/html/html5ever.rs
@@ -69,6 +69,14 @@ impl TokenSink for LinkExtractor {
                     }
                 }
 
+                // Check and exclude rel=preconnect. Other than prefetch and preload,
+                // preconnect only does DNS lookups and might not be a link to a resource
+                if let Some(rel) = attrs.iter().find(|attr| &attr.name.local == "rel") {
+                    if rel.value.contains("preconnect") {
+                        return TokenSinkResult::Continue;
+                    }
+                }
+
                 for attr in attrs {
                     let urls = LinkExtractor::extract_urls_from_elem_attr(
                         &attr.name.local,
@@ -136,6 +144,8 @@ impl LinkExtractor {
         // and https://html.spec.whatwg.org/multipage/indices.html#attributes-1
 
         match (elem_name, attr_name) {
+            // TODO: Skip <link rel="preconnect">
+
             // Common element/attribute combinations for links
             (_, "href" | "src" | "cite" | "usemap")
             // Less common (but still valid!) combinations
@@ -352,5 +362,15 @@ mod tests {
         let expected = vec![];
         let uris = extract_html(input, false);
         assert_eq!(uris, expected);
+    }
+
+    #[test]
+    fn test_skip_preconnect() {
+        let input = r#"
+            <link rel="preconnect" href="https://example.com">
+        "#;
+
+        let uris = extract_html(input, false);
+        assert!(uris.is_empty());
     }
 }
